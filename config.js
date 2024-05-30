e5a731838f69d88790757ca0ba8df5773b7121b6
Lê Phi Hà <85310045+lephiha@users.noreply.github.com>
2024-05-31 02:13:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,9 @@ const CONFIG = {
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    Đằng sau chữ yêu đây là thương
+            - Lê Phi Hà - `,
+    btnIntro: 'Ấn vào đi bé :vv',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
